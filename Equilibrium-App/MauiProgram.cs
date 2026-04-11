4a44bf12f57bb3cd7c85002f97c5efdcf524968e
Tim Vogel <144105942+SocialMediaIsBad@users.noreply.github.com>
2026-04-11 22:19:22 +0200

    Migrated my configuration parameters from a hard coded .cs file to json. Integrated them in the project.

--- Equilibrium-App/MauiProgram.cs
+++ Equilibrium-App/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace Equilibrium_App
 {
@@ -15,8 +17,19 @@ namespace Equilibrium_App
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
+            var assembly = Assembly.GetExecutingAssembly();
+            using var stream = assembly.GetManifestResourceStream("Equilibrium_App.appsettings.json");
+            var config = new ConfigurationBuilder()
+                .AddJsonStream(stream)
+                .Build();
+            builder.Configuration.AddConfiguration(config);
+            builder.Services.Configure<Configuration.AppSettings>(
+                builder.Configuration.GetSection("AppSettings"));
+
+            builder.Services.AddSingleton<MainPage>();
+
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
